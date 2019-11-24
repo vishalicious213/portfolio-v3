5a0ee5a8bf5944c031376f21e171b73037185cf8
vishalicious213 <vishalicious213@gmail.com>
2019-11-24 07:57:40 -0500

    basic header with styled component variables created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
+    <Header />
+    // <div className="App">
 
-    </div>
+    // </div>
   );
 }
 
