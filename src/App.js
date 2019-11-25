b36ccc67ffa4b1e16fd6c5a6e348c5b0f977bd8a
vishalicious213 <vishalicious213@gmail.com>
2019-11-25 02:24:24 -0500

    selected works text section created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Frontend from './About';
+import Selected from './Selected';
 
 function App() {
   return (
     <div>
       <Header />
       <Frontend />
+      <Selected />
     </div>
   );
 }
