58e6a87468e89a61078fda6197c7b817e6480a65
Vish Singh <vishalicious213@gmail.com>
2020-11-06 01:49:56 -0800

    added target='_blank' rel='noopener noreferrer' to social media links in Header

--- src/Header.js
+++ src/Header.js
@@ -13,13 +13,13 @@ function Header() {
               <SubheadingMobile className='SubheadingMobile'>FULL-STACK INTERLOPER</SubheadingMobile>
           </div>
           <Social>
-              <a href="https://www.linkedin.com/in/vish213/" title="LinkedIn"><i className="fab fa-linkedin"></i></a>
-              <a href="https://github.com/vishalicious213" title="GitHub"><i className="fab fa-github"></i></a>
-              <a href="https://neophyte.home.blog/" title="Wordpress"><i className="fab fa-wordpress"></i></a>
-              <a href="https://twitter.com/vishalicious213" title="Twitter"><i className="fab fa-twitter"></i></a>
-              {/* <a href="https://www.facebook.com/vish213" title="Facebook"><i className="fab fa-facebook-square"></i></a>
-              <a href="https://www.instagram.com/vishalicious213/" title="Instagram"><i className="fab fa-instagram"></i></a> */}
-              <a href="mailto:vishalicious213@gmail.com" title="Gmail"><i className="fas fa-envelope"></i></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/vish213/" title="LinkedIn"><i className="fab fa-linkedin"></i></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/vishalicious213" title="GitHub"><i className="fab fa-github"></i></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://neophyte.home.blog/" title="Wordpress"><i className="fab fa-wordpress"></i></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/vishalicious213" title="Twitter"><i className="fab fa-twitter"></i></a>
+              {/* <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/vish213" title="Facebook"><i className="fab fa-facebook-square"></i></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/vishalicious213/" title="Instagram"><i className="fab fa-instagram"></i></a> */}
+              <a target="_blank" rel="noopener noreferrer" href="mailto:vishalicious213@gmail.com" title="Gmail"><i className="fas fa-envelope"></i></a>
           </Social>
         </HeaderContainer>
       </PageHeader>
