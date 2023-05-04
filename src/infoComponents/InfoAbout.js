30ee496fabd9094251921e9f6b930f9b8f00f12f
vishalicious213 <vishalicious213@gmail.com>
2023-05-04 15:00:43 -0700

    updated infoAbout

--- src/infoComponents/InfoAbout.js
+++ src/infoComponents/InfoAbout.js
@@ -13,7 +13,8 @@ function About() {
                     <div className="sectionInfo">
                         <h3>Father, Bass Enthusiast, Extreme Metaller</h3>
                         <p>Intertwined with my coding interests are my role as a father, my study of electric bass (its not a guitar!) and my love of extreme metal, food, air frying and the Instant Pot. Years ago, I was a sprinter, tabletop gamer, ran a Renegade BBS, read for leisure instead of work and collected comic books.</p>
-                        <p>These days, when there's time, I blog and unsuccessfully try to keep my distance from Nick Jr, Disney Junior, Roblox videos and marathons of Winx Club while being outnumbered by my wife and two girls. I also walk a 5k several times a week. Soon, I'll run it.</p>
+                        <p>These days, when there's time, I blog, attend concerts, hunt down new eateries and unsuccessfully try to keep my distance from Nick Jr, Disney Junior, Roblox videos, a deluge of kids' YouTube channels and marathons of Winx Club while being outnumbered by my two girls.</p>
+                        <p>I also walk a 5k several times a week. Soon, I'll run it.</p>
                     </div>
                     <div className="pictureFrame">
                         <img src={require('../img/v5.jpg')} alt="Older daughter playing guitar and younger daughter playing drums."></img>
